3e032418d43771748db76866d56f3c7bdb868f40
unknown <german.af86@gmail.com>
2024-04-03 19:01:57 +0200

    vercel

--- frontNg/src/app/services/users.service.ts
+++ frontNg/src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environment';
+//import { environment } from 'src/environment';
 
 
 @Injectable({
@@ -13,6 +13,7 @@ export class UsersService {
   constructor(private service:HttpClient) { }
 
   GetUsers(): Observable<any> {
-    return this.service.get(`${environment.hostname}/users`)
+    //return this.service.get(`${environment.hostname}/users`)
+    return this.service.get(` http://localhost:3001/users`)
   }
 }
