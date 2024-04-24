a77dd5cb77079d564e0e84c0363cb84f382ad3ee
unknown <german.af86@gmail.com>
2024-04-24 17:12:18 +0200

    openai a posto

--- back/src/handlers/recHandler.js
+++ back/src/handlers/recHandler.js
@@ -1,27 +1,29 @@
-const { OPENIA_KEY } = process.env
+require('dotenv').config();
+const { OPENAI_KEY } = process.env
+
+const text = 'qué recomendación sencilla me puedes brindar para un aestablecimiento agropecuario con las siguientes características:'
 
 const getAnswerFromAPI = async (req, res) => {
   const { area, ubication, class_, slope, rainfall, rainfallb, weeding, compactation, erosion } = req.body;
-  const prompt = `dónde queda ${ubication}`
-  const api = await fetch('https://api.openai.com/v1/chat/completions', {
+  const prompt = `${text} ubicación: ${ubication}, superficie: ${area} hectáreas, aptitud agrícola: ${class_}, pendiente: ${slope}%, precipitaciones esperadas y anteriores (milímetros):${rainfall} y ${rainfallb}, enmalezamiento: ${weeding}(%), compactación: ${compactation}, erosión: ${erosion}?`
+  const api = await fetch('https://api.openai.com/v1/completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${OPENIA_KEY}`
+      'Authorization': `Bearer ${OPENAI_KEY}`
     },
     body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      prompt: prompt
+      model: 'gpt-3.5-turbo-instruct',
+      prompt: prompt,
+      max_tokens: 200,
+      //temperature: 0.9
     })
   })
-
   console.log(prompt)
-  const resp = api.statusText
-  console.log(api)
-  res.status(200).json(resp)
-
+  const respAwait = await api.json()
+  console.log(respAwait)
+  res.status(200).json(respAwait.choices[0].text)
 }
-
 
 module.exports = {
   getAnswerFromAPI
